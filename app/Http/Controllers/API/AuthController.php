7f325fc376dd17de5fb969dc9f38cf20071e6f05
newkoo <runjeberke@gmail.com>
2022-12-07 17:56:19 +0300

    Auth Backend Added and user model updated

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $msg = "Hatalı Giriş!";
+        $success = false;
+        $data = [];
+        $errors = [];
+        $http_code = 200;
+        $input = $request->all();
+        //validator
+        $validator = Validator::make($input, [
+            "name" => "required",
+            "email" => "required|email|unique:App\Models\User,email",
+            "password" => "required",
+            "repass" => "required|same:password"
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            $http_code = 400;
+        } else {
+            $input['password'] = bcrypt($input['password']);
+            $user = User::create($input);
+            $data = [
+                'token' => $user->createToken('MyApp')->plainTextToken,
+                'name' => $user->name
+            ];
+            $msg = "Kayıt Başarılı";
+            $success = true;
+        }
+
+        $response = [
+            'msg' => $msg,
+            'success' => $success,
+            'data' => $data,
+            'errors' => $errors
+        ];
+
+        return response()->json($response, $http_code);
+    }
+
+    public function login(Request $request)
+    {
+        $msg = "Giriş Bilgileriniz Yanlış";
+        $success = false;
+        $data = [];
+        $http_code = 400;
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            //$user = Auth::user();
+            $user = $request->user();
+            $data = [
+                'token' => $user->createToken('MyApp')->plainTextToken,
+                'name' => $user->name
+            ];
+            $success = true;
+            $http_code = 200;
+            $msg = "Giriş Başarılı";
+        }
+        $response = [
+            'msg' => $msg,
+            'success' => $success,
+            'data' => $data,
+        ];
+        return response()->json($response, $http_code);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function checkMail($mail)
+    {
+        return self::where('email', $mail)->first() ? true : false;
+    }
 }
