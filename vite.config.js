3ace4242298a190aa9bb998abfcea7928bc28d87
newkoo <runjeberke@gmail.com>
2022-12-08 00:46:38 +0300

    volar config added

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import Preview from 'vite-plugin-vue-component-preview';
+import Inspect from 'vite-plugin-inspect';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +10,8 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        Inspect(),
+        Preview(),
         vue(),
     ],
 });
